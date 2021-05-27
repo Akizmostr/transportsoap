17b4751adad59ee8afff6a67ab393f9fb60f11cc
Akizmostr <nterehina666@mail.ru>
2021-05-27 14:07:11 +0300

    connection added

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,12 +1,41 @@
 package com.company;
 
 import javax.xml.soap.*;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
+    // login data
+    private static final String KODF = "martinkolek";
+    private static final String USERNAME = "asstra";
+    private static final String PASS = "asstra1";
 
     public static void main(String[] args) {
 	// write your code here
+
+        try{
+            // create a connection for SOAP
+            SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();
+            SOAPConnection soapConnection = soapConnectionFactory.createConnection();
+
+            // calling parameters of function_getCarsList ()
+            Map<String, String>parametersCarList = new HashMap<>();
+            parametersCarList.put("kodf", KODF);
+            parametersCarList.put("username", USERNAME);
+            parametersCarList.put("pass", PASS);
+            // SOAP server URL
+            String url = "https://api.webdispecink.cz/code/WebDispecinkServiceNet.php?wsdl";
+
+            // create a request with parameter for calling the function _getCarsList ()
+            SOAPMessage soapRequestCarList = createSOAPRequest("_getCarsList", parametersCarList);
+            // calling the function_getCarsList () to get the car registration number
+            SOAPMessage soapResponseCarList = soapConnection.call(soapRequestCarList, url);
+
+        }
+        catch(Exception e){
+            System.err.println("Error occurred while handling SOAP Request.");
+            e.printStackTrace();
+        }
     }
 
     private static SOAPMessage createSOAPRequest(String functionName, Map<String, String> parameters) throws Exception
@@ -32,6 +61,11 @@ public class Main {
         }
 
         soapMessage.saveChanges();
+
+        /* Print the request message, just for debugging purposes */
+        System.out.println("Request SOAP Message:");
+        soapMessage.writeTo(System.out);
+        System.out.println("\n");
 
         return soapMessage;
     }
